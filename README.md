7d8c7f8728d8158ac2753bc8c3005132fa4394dc
jenhaochen <d06922025@csie.ntu.edu.tw>
2019-04-15 13:54:17 +0800

    README

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ConvS2P is a molecular property prediction model built based on [ConvS2S](https://github.com/pytorch/fairseq).
 
 ![Model](convS2P.png)
+
+### Usage
+Train: ./myRun.sh 1
+Generation: ./myGen.sh 
--- myGen.sh
+++ myGen.sh
@@ -1,5 +1,5 @@
 #MODEL_DIR=/data/jenhaochen/delaney/s_e_cp/
-MODEL_DIR=/home/arucuid/explain/fairseq/checkpoints/fconv/
+MODEL_DIR=./checkpoints/fconv/
 
 python interactive.py --cpu --path $MODEL_DIR/checkpoint_last.pt $MODEL_DIR
 
